c8070d5f8d3d3937ca992263a9727d41db83311e
Sabareesh-SS <sabareesh3898@gmail.com>
2025-02-15 09:32:07 +0530

    updated

--- src/pages/MarkEntry.jsx
+++ src/pages/MarkEntry.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "./../styles/markentry.css";
 
-// import "./MarkEntry.css";
    
 const MarkEntry = () => {
   const [marks, setMarks] = useState({
